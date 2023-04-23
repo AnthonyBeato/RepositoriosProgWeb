c5ee8b296f894fb5838e8a1fadefd12bd230d2bd
axba0001 <axba0001@ce.pucmm.edu.do>
2023-04-23 16:10:57 -0400

    arreglos minimos

--- final/src/main/java/org/example/grpc/CreacionService.java
+++ final/src/main/java/org/example/grpc/CreacionService.java
@@ -2,11 +2,50 @@ package org.example.grpc;
 
 import acortadorrn.AcortadorRn;
 import acortadorrn.ServicioCreacionGrpc;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import io.grpc.stub.StreamObserver;
+import org.example.encapsulacion.URL;
+
+import java.time.Instant;
+
 
 public class CreacionService extends ServicioCreacionGrpc.ServicioCreacionImplBase {
     @Override
     public void crearURL(AcortadorRn.CrearURLRequest request, StreamObserver<AcortadorRn.CrearURLResponse> responseObserver) {
-        // implementa aquí la lógica para crear una nueva URL y obtener sus estadísticas y vista previa
+//        // Obtener el JSON del cuerpo de la solicitud
+//        JsonObject jsonObject = JsonParser.parseString(request.getJson()).getAsJsonObject();
+//
+//        // Obtener los valores de los campos del JSON
+//        String id_usuario = jsonObject.get("id_usuario").getAsString();
+//        String url_original = jsonObject.get("url_original").getAsString();
+//
+//        // Generar la URL acortada
+//        String id_url = generarIdURL();
+//        String url_acortada = URL_BASE + id_url;
+//
+//        // Generar la fecha de creación
+//        Instant instant = Instant.now();
+//        Timestamp fecha_creacion = Timestamp.newBuilder()
+//                .setSeconds(instant.getEpochSecond())
+//                .setNanos(instant.getNano())
+//                .build();
+//
+//        // Construir el objeto de estadísticas (en este ejemplo se deja vacío)
+//        EstadisticasURL estadisticasURL = EstadisticasURL.newBuilder().build();
+//
+//        // Construir la respuesta con los datos de la nueva URL acortada
+//        AcortadorRn.CrearURLResponse response = CrearURLResponse.newBuilder()
+//                .setUrlOriginal(url_original)
+//                .setUrlAcortada(url_acortada)
+//                .setFechaCreacion(fecha_creacion)
+//                .setEstadisticas(estadisticasURL)
+//                .build();
+//
+//        // Enviar la respuesta al cliente
+//        responseObserver.onNext(response);
+//        responseObserver.onCompleted();
     }
 }
--- final/src/main/java/org/example/grpc/ListadoService.java
+++ final/src/main/java/org/example/grpc/ListadoService.java
@@ -3,12 +3,40 @@ package org.example.grpc;
 
 import acortadorrn.AcortadorRn;
 import acortadorrn.ServicioListadoGrpc;
+import com.google.gson.Gson;
 import io.grpc.stub.StreamObserver;
+import org.example.encapsulacion.Acortador;
+import org.example.encapsulacion.Usuario;
+import org.example.servicios.ServiciosAcortador;
+import org.example.servicios.ServiciosUsuario;
+
+import com.google.protobuf.ByteString;
+
+
+import java.util.List;
 
 public class ListadoService extends ServicioListadoGrpc.ServicioListadoImplBase {
 
     @Override
     public void listarURLs(AcortadorRn.ListarURLsRequest request, StreamObserver<AcortadorRn.ListarURLsResponse> responseObserver) {
-        // implementa aquí la lógica para listar las URLs y sus estadísticas para un usuario
+        String idUsuario = request.getIdUsuario();
+        Usuario usuario = ServiciosUsuario.getInstancia().find(idUsuario);
+
+        List<Acortador> listaDeAcortadores = ServiciosAcortador.getInstancia().findAllByUser(usuario);
+
+        // Creamos una instancia de Gson
+        Gson gson = new Gson();
+
+        // Convertimos la lista de objetos Java a una cadena JSON utilizando Gson
+        String json = gson.toJson(listaDeAcortadores);
+
+        // Creamos la respuesta para enviar al cliente
+        AcortadorRn.ListarURLsResponse response = AcortadorRn.ListarURLsResponse.newBuilder()
+                .setJson(json)
+                .build();
+
+        // Enviamos la respuesta al cliente
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 }
